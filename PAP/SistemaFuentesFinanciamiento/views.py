5b3dfd1d2e61153a7dd98552b76180687929e510
MauLTamez <mauricioltamez@gmail.com>
2023-07-13 10:29:22 -0600

    Visualización pantalla de salida

--- PAP/SistemaFuentesFinanciamiento/views.py
+++ PAP/SistemaFuentesFinanciamiento/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Fuente
+from django.utils.translation import gettext as _
 
 # Create your views here.
+
+def home(request):
+    fuentes = Fuente.objects.all()
+    return render(request, "salida.html", {"fuentes": fuentes})
